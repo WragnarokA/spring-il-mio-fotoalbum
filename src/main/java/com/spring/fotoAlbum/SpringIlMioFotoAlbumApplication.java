1bc30d284c98e0f7ae9933f5d05c7d5625b8a4db
wilmer <wilmer.arango.a@gmail.com>
2023-11-23 14:35:59 +0100

    1

---
+++ src/main/java/com/spring/fotoAlbum/SpringIlMioFotoAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.spring.fotoAlbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIlMioFotoAlbumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIlMioFotoAlbumApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/spring/fotoAlbum/model/Photo.java
@@ -0,0 +1,7 @@
+package com.spring.fotoAlbum.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Photo {
+}
